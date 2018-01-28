a23eadccae4a4fec17a38d08b91b18bf4f551bc1
Wayne Arliss <warliss@gmail.com>
2018-01-28 12:31:22 +0000

    Update to script Get-DefaultGateway.ps1

--- Scripts/Get-DefaultGateway.ps1
+++ Scripts/Get-DefaultGateway.ps1
@@ -1,12 +1,12 @@
-﻿#####
+﻿################################################################################
 #
 # Get-DefaultGateway
 # 
-#####
+################################################################################
 #
 # Author: Wayne Arliss
 #
-#####
+################################################################################
 #
 # Description
 # takes a computer name and checks for the default gateway on 
@@ -17,18 +17,19 @@
 # If neither parameters are passed the script with default to the local
 # computer name Env:\COMPUTERNAME
 #
-#####
+################################################################################
 #
 # Version History
 #
 # Date       |  V   | Notes
 # 2017/03/17 |  0.a | Initial version
-# 2017/03/21 |  0.1 | Rewritten GetGateway function to add details to an object for output
+# 2017/03/21 |  0.1 | Rewritten GetGateway function to add details to an object 
+#                     for output
 #
 #
 #
-#####
-
+################################################################################
+[cmdletbinding()]
 Param(
   [string]$ComputerName = $env:COMPUTERNAME,
   [string]$FilePath
@@ -41,20 +42,43 @@ function GetGateway([string]$computer)
     Where-Object defaultipgateway -NotLike ""#>
     
     # Collect WMI Objects
-    $NetworkAdapterConfiguration = Get-WmiObject win32_NetworkAdapterConfiguration -ComputerName $computer | Where-Object defaultipgateway -NotLike ""
-    $OutputArray += New-Object PSObject -Property @{HostName=$computer; 
-        IPAddress=$NetworkAdapterConfiguration.IPAddress; 
-        DefaultGateway=$NetworkAdapterConfiguration.DefaultIPGateway;
-        Index=$NetworkAdapterConfiguration.Index;
+    try {
+        $NetworkAdapterConfiguration = Get-WmiObject win32_NetworkAdapterConfiguration -ComputerName $computer | Where-Object defaultipgateway -NotLike ""
+        $OutputArray += New-Object PSObject -Property @{HostName=$computer
+        IPAddress=$NetworkAdapterConfiguration.IPAddress
+        DefaultGateway=$NetworkAdapterConfiguration.DefaultIPGateway
+        Index=$NetworkAdapterConfiguration.Index
         MACAddress=$NetworkAdapterConfiguration.MACAddress}
+    }
+    catch {
+        $ErrorMessage = $_.Exception.Message
+        $FailedItem = $_.Exception.ItemName
+        Write-Verbose("Failed Item: $FailedItem")
+        Write-Host("Error: $ErrorMessage")
+        Break
+    }
     $OutputArray
 }
 
 $OutputArray=@()
 if(!$FilePath)
 {
-    Write-Verbose("No FilePath passed")
-    GetGateway($ComputerName)
+    Write-Verbose("No FilePath passed... Using -computer parameter")
+    try {
+        Write-Verbose("Getting Gateway...")
+        GetGateway($ComputerName)
+        Write-Verbose("Done getting Gateway...")
+    }
+    catch {
+        $ErrorMessage = $_.Exception.Message
+        $FailedItem = $_.Exception.ItemName
+        if($ErrorMessage -like "Access is denied.*"){
+            Write-Host("Permission denied for $computer, try running from Domain Admin account")
+        } else {
+            Write-Host("Error: $ErrorMessage")
+        }
+        Break
+    }
 } else {
     Write-Verbose("FilePath passed")
     $Lines = Get-Content($FilePath)
@@ -77,10 +101,9 @@ if(!$FilePath)
             }
             Break
         }
-        <#
         finally
         {
             Write-host("Done and dusted...")
-        }#>
+        }
     }
 }

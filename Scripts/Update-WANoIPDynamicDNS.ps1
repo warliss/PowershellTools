f9089691398f529a4d34594121c255dd56f95f1d
Wayne Arliss <warliss@gmail.com>
2018-01-29 08:21:34 +0000

    Minor updates to Update-WANoIPDynamicDNS.ps1

--- Scripts/Update-WANoIPDynamicDNS.ps1
+++ Scripts/Update-WANoIPDynamicDNS.ps1
@@ -2,22 +2,30 @@
 Parameters:-
 hostname - from input parameters
 ip - taken from 'https://ifcfg.me/ip'
+
+Things still to be done
+    1. Error handling for both the Invoke-WebRequest lines
+    2. Check if credential file exists
+
 #>
+[cmdletbinding()]
 Param(
   [string]$HostName = 'warliss.hopto.org',
-  [string]$filePath,
-  [string]$credential
+  [string]$credentialPath = '.\credential.xml'
 )
-[cmdletbinding()]
 
-$credential = Import-Clixml -Path .\credential.xml
-$ipFinder = (Invoke-WebRequest('https://ifcfg.me/ip'))
+$credential = Import-Clixml -Path $credentialPath
+$ipFinder = (Invoke-WebRequest('https://xxifcfg.me/ip'))
 
 if($ipFinder.StatusCode -eq 200){
+    Write-Verbose("StatusCode is good...")
     $ipAddress = $ipFinder.content
-    $update = (Invoke-WebRequest('https://dynupdate.no-ip.com/nic/update?hostname=' + $HostName +'&myip=' + $ipAddress) -Credential $credential).content
+    Write-Verbose("IP Address is $ipAddress")
+    $update = (Invoke-WebRequest('https://dynupdate.no-ip.com/nic/update?hostname=' + $HostName +'&myip=' + $ipAddress) -Credential $credential)
+    write-host($update)
 }else{
-    Write-Host("Nope...")
+    $ErrorStatusCode = $ipFinder.StatusCode
+    Write-Host("Error - $ErrorStatusCode")
 }
 
 #$ipAddress = $ipFinder.content

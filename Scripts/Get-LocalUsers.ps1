989a481b5248f20e77ea7e84f857bb9e3e077d13
Wayne Arliss <WayneArliss@seachill.com>
2018-02-20 11:40:07 +0000

    Update

---
+++ Scripts/Get-LocalUsers.ps1
@@ -0,0 +1,12 @@
+﻿function Get-LocalUser ($Computername = $env:COMPUTERNAME) {
+    Get-WmiObject -Query "Select * from Win32_UserAccount Where LocalAccount = 'True'" -ComputerName $ComputerName |
+    Select-Object Name, Disabled, Caption
+}
+
+## Build a list of computers
+$ComputerList = Get-ADComputer -SearchBase "OU=Servers,OU=CHILLED,OU=SEACHILL,DC=seachill,DC=com" -Filter * | Select-Object Name | Sort-Object Name
+foreach ($Computer in $ComputerList){
+    $Computer
+    Get-LocalUser -ComputerName $Computer
+    }
+
--- Scripts/Get-ServiceDetails.ps1
+++ Scripts/Get-ServiceDetails.ps1
@@ -1,24 +1,45 @@
-﻿##Get-Service -ComputerName xxxx -Name TeamViewer##
-$ComputerList = 'seachw011', 'seachw010', 'seachearnie', 'seaisys'
+﻿[cmdletbinding()]
+param (
+    [parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+    [string]$ComputerFileName,
+    [string[]]$ComputerList = $env:COMPUTERNAME,
+    [string]$ServiceName = 'TeamViewer',
+    [string]$OutputFileName = "h:\temp.csv"
+)
+
+if ($ComputerFileName){
+    if(Test-Path -Path $ComputerFileName){
+        $ComputerList = Get-Content -Path $ComputerFileName
+    }
+}
 
 foreach ($Computer in $ComputerList){
+    ## Create the output object
+    $OutputObject = New-Object -TypeName psobject
+
+    # Add the computer name to the object
+    $OutputObject | Add-Member -MemberType NoteProperty -Name ComputerName -Value $Computer
+
+    # Check the if the computer is online
     if (Test-Connection -ComputerName $Computer -Count 1 -Quiet){
-        $ServiceResult = Get-Service -ComputerName $Computer -Name TeamViewer
-        $OutputObject = New-Object -TypeName psobject
-        $OutputObject | Add-Member -MemberType NoteProperty -Name ComputerName -Value $Computer
+        Write-Verbose ("$Computer is online")
+        # Get the details of the service
+        $ServiceResult = Get-Service -ComputerName $Computer -Name $ServiceName
+
+        # Add the results to the output object
         $OutputObject | Add-Member -MemberType NoteProperty -Name ComputerStatus -Value "Online"
         $OutputObject | Add-Member -MemberType NoteProperty -Name ServiceName -Value $ServiceResult.Name
         $OutputObject | Add-Member -MemberType NoteProperty -Name DisplayName -Value $ServiceResult.DisplayName
         $OutputObject | Add-Member -MemberType NoteProperty -Name ServiceStatus -Value $ServiceResult.Status
-        $OutputObject
     } else {
-        $OutputObject = New-Object -TypeName psobject
-        $OutputObject | Add-Member -MemberType NoteProperty -Name ComputerName -Value $Computer
+        Write-Verbose ("$Computer is offline")
+        # Add the results to the output object
         $OutputObject | Add-Member -MemberType NoteProperty -Name ComputerStatus -Value "Offline"
         $OutputObject | Add-Member -MemberType NoteProperty -Name ServiceName -Value $null
         $OutputObject | Add-Member -MemberType NoteProperty -Name DisplayName -Value $null
         $OutputObject | Add-Member -MemberType NoteProperty -Name ServiceStatus -Value $null
-        $OutputObject
-        #Write-Host "$Computer - Not Running"
     }
+    ## Export to CSV at this point 
+    $OutputObject | Export-Csv -Path $OutputFileName -Append -NoTypeInformation
+    #Write-Verbose $OutputObject
 }

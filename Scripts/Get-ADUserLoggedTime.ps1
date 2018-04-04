d991bc8e108282e3fbe140ad4686b46709787d55
Wayne Arliss <WayneArliss@seachill.com>
2018-04-04 16:36:33 +0100

    New Scripts

---
+++ Scripts/Get-ADUserLoggedTime.ps1
@@ -0,0 +1,4 @@
+﻿$OutputFile = 'h:\ADUserLastLogin_Coated.csv'
+Get-ADUser -Filter * -SearchBase "OU=Users,OU=COATED,OU=SEACHILL,DC=seachill,DC=com" -ResultPageSize 0 -Prop CN,samaccountname,lastLogonTimestamp,UserPrincipalName,Enabled | 
+    Select CN,samaccountname,@{n="lastLogonDate";e={[datetime]::FromFileTime($_.lastLogonTimestamp)}},UserPrincipalName,Enabled |
+    Export-CSV -NoType -Path $OutputFile
---
+++ Scripts/Get-WAADGroupMembers.ps1
@@ -0,0 +1,4 @@
+﻿$ADGroupDetails = Get-ADGroupMember "vpn_coated"
+foreach ($Detail in $ADGroupDetails){
+    $Detail | Export-Csv -Path "H:\VPNUsers_Coated.csv" -NoTypeInformation -Append
+}

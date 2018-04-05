6ef84e10c4916ca02caf745bf7c7cb41215b8cfb
Wayne Arliss <WayneArliss@seachill.com>
2018-04-05 11:21:53 +0100

    New Updates

--- Scripts/Get-ADUserLoggedTime.ps1
+++ Scripts/Get-ADUserLoggedTime.ps1
@@ -1,4 +1,8 @@
 ﻿$OutputFile = 'h:\ADUserLastLogin_Coated.csv'
-Get-ADUser -Filter * -SearchBase "OU=Users,OU=COATED,OU=SEACHILL,DC=seachill,DC=com" -ResultPageSize 0 -Prop CN,samaccountname,lastLogonTimestamp,UserPrincipalName,Enabled | 
+$SearchBase = "OU=Users,OU=CHILLED,OU=SEACHILL,DC=seachill,DC=com"
+Get-ADUser -Filter * -SearchBase "OU=Users,OU=CHILLED,OU=SEACHILL,DC=seachill,DC=com" -ResultPageSize 0 -Prop CN,samaccountname,lastLogonTimestamp,UserPrincipalName,Enabled | 
     Select CN,samaccountname,@{n="lastLogonDate";e={[datetime]::FromFileTime($_.lastLogonTimestamp)}},UserPrincipalName,Enabled |
     Export-CSV -NoType -Path $OutputFile
+
+$OutputObject = Get-ADUser -Filter * -SearchBase $SearchBase
+$OutputObject | Sort-Object -Property Surname | Export-Csv -Path

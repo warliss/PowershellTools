20ab560e01d794a0088c6a22a1aaf927141b9856
Wayne Arliss <warliss@gmail.com>
2018-06-05 08:19:12 +0100

    Update from home

--- Scripts/Connect-ExchangeOnline.ps1
+++ Scripts/Connect-ExchangeOnline.ps1
@@ -3,3 +3,12 @@
     $global:office365= New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $office365Credential  -Authentication Basic   –AllowRedirection
     Import-PSSession $office365
 }
+
+function Disconnect-ExchangeOnline ($SessionID){
+    if ($SessionID -ne $null) {
+        
+    }
+}
+
+Write-Host ('Connecting - Need details...')
+Connect-ExchangeOnline
